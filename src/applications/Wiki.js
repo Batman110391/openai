3a7de2f76389d08f3eb216f256276ae4a34193ad
Simone Gattinara <simone.gattinara@smc.it>
2022-11-08 21:30:56 +0100

    fix

--- src/applications/Wiki.js
+++ src/applications/Wiki.js
@@ -1,9 +1,28 @@
 import React from "react";
+//import scrapeHtmlWeb from "scrape-html-web";
+const options = {
+  urls: ["http://nodejs.org/"],
+  directory: "/path/to/save/",
+};
+
+// with async/await
+
+const optionsFirstExample = {
+  url: "https://nodejs.org/en/blog/",
+  mainSelector: ".blog-index",
+  childrenSelector: [
+    { key: "date", selector: "time", type: "text" },
+    { key: "version", selector: "a", type: "text" },
+    { key: "link", selector: "a", attr: "href" },
+  ],
+};
 
 export default function Wiki() {
   React.useEffect(() => {
     (async () => {
-      console.log("hi");
+      //const data = await scrapeHtmlWeb(optionsFirstExample);
+      //const result = await scrape(options);
+      console.log("example 1 :");
     })();
   }, []);
 
